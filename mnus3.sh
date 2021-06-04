f4f3b93ccfb3c4177d694ca9a513eb7c8549311a
Diego E. KESSELMAN <42894928+dkesselman@users.noreply.github.com>
2021-06-03 19:24:34 -0500

    New upload with some fixes, multi-threading & 7zip
    New upload with some fixes, multi-threading and 7zip compression
    You can list content from cloud without downloading the full SAVF, just the content

--- mnus3.sh
+++ mnus3.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+QIBM_MULTI_THREADED='Y'
+export QIBM_MULTI_THREADED
+
+
 source mnus3_const.sh
 source mnus3_inc.sh
 
@@ -11,6 +15,8 @@ source mnus3_inc.sh
         2) S=M2;SC;if [[ $cur == "" ]];then R;$e "\n$( BUCKETGT  )\n";ES;fi;;
         3) S=M3;SC;if [[ $cur == "" ]];then R;$e "\n$( BUCKETDL  )\n";ES;fi;;
         4) S=M4;SC;if [[ $cur == "" ]];then R;$e "\n$( BKPTOCLD  )\n";ES;fi;;
-        5) S=M5;SC;if [[ $cur == "" ]];then R;$e "\n$($e ICOS Menu by ESSELWARE )\n";ES;fi;;
-        6) S=M6;SC;if [[ $cur == "" ]];then R;exit 0;fi;;
+        5) S=M5;SC;if [[ $cur == "" ]];then R;$e "\n$( BKPTOCLD2 )\n";ES;fi;;  
+        6) S=M6;SC;if [[ $cur == "" ]];then R;$e "\n$( LSTCLDBKP )\n";ES;fi;;  
+        7) S=M7;SC;if [[ $cur == "" ]];then R;$e "\n$($e IBM COS Menu by ESSELWARE )\n";ES;fi;;
+        8) S=M8;SC;if [[ $cur == "" ]];then R;exit 0;fi;;
  esac;POS;done
--- mnus3_const.sh
+++ mnus3_const.sh
@@ -1,5 +1,9 @@
-        E='echo -e';e='echo -en';trap "R;exit" 2                                                                                                                              
-    S3CMD='/QOpenSys/bin/s3cmd'                                                                                                                                              
-BUCKETDFT='s3://mybackup/'                                                                                                                                                     
-  IFSPATH='/backup2cloud/'                                                                                                                                                    
+        E='echo -e';e='echo -en';trap "R;exit" 2             
+    S3CMD='aws --endpoint-url=https://s3.us-south.cloud-object-storage.appdomain.cloud s3 '
+  IFSPATH='/backup2cloud'      
+   LIBLST=$IFSPATH'/liblist.lst'     
+  FLISTHD='System-- Save Date/Time Object--- Type---- Attribute- Size (Bytes)---- Owner------ Description--------------' 
       ESC=$( $e "\e")
+num_procs=3
+   pgzthr=12
+  maxsize=20
