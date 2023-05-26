0dec91e36f7d3eced6055f2e7f4590cf7e2a7ea4
Diego E. KESSELMAN <42894928+dkesselman@users.noreply.github.com>
2023-05-26 06:20:41 -0600

    Delete mnus3_const.sh
    Deprecated option

--- mnus3_const.sh
+++
@@ -1,9 +0,0 @@
-        E='echo -e';e='echo -en';trap "R;exit" 2             
-    S3CMD='aws --endpoint-url=https://s3.us-south.cloud-object-storage.appdomain.cloud s3 '
-  IFSPATH='/backup2cloud'      
-   LIBLST=$IFSPATH'/liblist.lst'     
-  FLISTHD='System-- Save Date/Time Object--- Type---- Attribute- Size (Bytes)---- Owner------ Description--------------' 
-      ESC=$( $e "\e")
-num_procs=3
-   pgzthr=12
-  maxsize=20
